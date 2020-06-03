df138c5ce9150a8c2f53ce55f44b4edf9f02ee1f
Will Bennion <will.bennion@outlook.com>
2020-06-03 13:31:06 +0100

    Added the ability to "group" tab controls together Any grouped tab controls will keep their indexes in sync. For example, this could be used to have all tab controls that show a script show it in the same language when the user changes language.

--- themes/aurora/assets/js/tabs-script.js
+++ themes/aurora/assets/js/tabs-script.js
@@ -1,21 +1,37 @@
 (function() {
     // Mark the the first tab content and header for every tabs element as active (easier to do via js than hugo)
-    document.querySelectorAll('.tabs .tab-content:first-child, .tabs .tab-header:first-child').forEach(el =>
-        el.classList.add('active'));
+    document.querySelectorAll('.tabs').forEach(el => setActiveIndex(el, 0));
 
     // Tab header button handler
     function tabHeaderClickHandler(e) {
         // Index of the clicked button in the tab headers
         let idx = [...this.parentElement.children].indexOf(this);
-        
-        // Try find the content element by this index
-        let trg = this.closest('.tabs').querySelector(`.tab-content:nth-child(${idx + 1})`);
+        let container = this.closest('.tabs');
+
+        // If the container has a group, find other tab controls with the same group name
+        if (container.dataset.tabsSyncGroup)
+            document.querySelectorAll(`.tabs[data-tabs-sync-group="${container.dataset.tabsSyncGroup}"]`).forEach(el =>
+                setActiveIndex(el, idx));
+
+        // If there is no group, just update this element only
+        else
+            setActiveIndex(container, idx);
+    }
+
+    /** Sets the active tab of the container to the given index
+     * @param {HTMLElement} container The .tabs container to set the active tab in
+     * @param {number} index The index of the tab to set active. */
+    function setActiveIndex(container, index) {
+        // Try find content at the target idx. It there is no content, do nothing
+        let trg = container.querySelector(`.tab-content:nth-child(${index + 1})`);
         if (trg != null) {
-            // If the target content exists, remove active from other contents and add to target
-            this.closest('.tabs').querySelectorAll('.tab-content.active, .tab-header.active').forEach(el =>
+            // Remove existing active classes
+            container.querySelectorAll('.tab-content.active, .tab-header.active').forEach(el =>
                 el.classList.remove('active'));
+
+            // Add active to target content and header
             trg.classList.add('active');
-            this.classList.add('active');
+            container.querySelector(`.tab-header:nth-child(${index + 1})`).classList.add('active');
         }
     }
 
